dcff7468b01b3268c2a3a3e8d5be25c1fa80e037
atulkumbhar85 <atul.kumbhar1985@gmail.com>
2022-07-14 17:31:09 +0530

    added fetch call to button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data,setData] = useState({name:"default"})
   const handleButtonClick = () =>{
-    window.alert("Hi")
+    const res = fetch("https://cloud.shriswamisamarth.site/test/api/user")
+    setData(res)
+    console.log(res);
   }
   return (
     <div className="App">
       <button onClick={handleButtonClick}>Click me</button>
+      <h1>{data.name}</h1>
     </div>
   );
 }
