a3865edc9c536a291ecce819738d70c054b7daa3
atulkumbhar85 <atul.kumbhar1985@gmail.com>
2022-07-15 11:05:40 +0530

    used axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [data,setData] = useState({name:"default"})
-  const handleButtonClick = () =>{
-    const res = fetch("https://cloud.shriswamisamarth.site/test/api/user")
-    setData(res)
-    console.log(res);
-  }
+  const [data, setData] = useState({ name: "default" });
+  const handleButtonClick = async () => {
+
+    axios.get("https://cloud.shriswamisamarth.site/test/api/user")
+    .then(response => {
+      console.log('====================================');
+      console.log(response.data);
+      console.log('====================================');
+      setData(response.data);
+    })
+    // const res = await fetch("https://cloud.shriswamisamarth.site/test/api/user");
+    
+    // console.log('====================================');
+    // console.log(res.json());
+    // console.log('====================================');
+      // .then((res) => {
+      //   if (res) {
+      //     console.log(res.json());
+      //     setData(res);
+      //   }
+      // })
+      // .catch((err) => {
+      //   console.log("====================================");
+      //   console.log(err);
+      //   console.log("====================================");
+      // });
+
+    // setData(res)
+    // console.log(res);
+  };
   return (
     <div className="App">
       <button onClick={handleButtonClick}>Click me</button>
